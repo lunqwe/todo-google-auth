38a8748ba22e6407311b2372f9d81d51d7ea2dae
lunqwe <roma.boyko10068@gmail.com>
2024-07-11 14:10:28 +0300

    model implementation

--- todo/models.py
+++ todo/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    pass
+    title = models.CharField(max_length=255)
+    description = models.Charfield(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self) -> str:
+        return f'{self.title} ({self.created_at})'
