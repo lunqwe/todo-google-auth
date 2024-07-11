7afd66093b2a700e10258fc7bf273ac9cfd3056c
lunqwe <roma.boyko10068@gmail.com>
2024-07-11 14:26:53 +0300

    updated model

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Todo(models.Model):
+    owner = owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=255)
     description = models.Charfield(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,4 +14,4 @@ class Todo(models.Model):
     completed = models.BooleanField(default=False)
     
     def __str__(self) -> str:
-        return f'{self.title} ({self.created_at})'
+        return f'{self.owner.username} - {self.title} ({self.created_at})'
