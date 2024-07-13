2ed15e716e5d75c9fcb2c061d4cead5e22b95df0
lunqwe <roma.boyko10068@gmail.com>
2024-07-14 02:15:53 +0300

    fixed url

--- todo/urls.py
+++ todo/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('get/<int:pk>', RetrieveTodoView.as_view(), name='retrieve-todo'),
     path('list/', TodoListView.as_view(), name='list-todo-view'),
     path('update/<int:pk>', UpdateTodoView.as_view(), name='update-todo-view'),
-    path('delete/<int:pk', DeleteTodoView.as_view(), name='delete-todo-view'),
+    path('delete/<int:pk>', DeleteTodoView.as_view(), name='delete-todo-view'),
 ]
